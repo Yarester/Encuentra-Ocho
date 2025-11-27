79018337541a6779172f09dd9bf9875730588ee1
Yarester <yar3ster@gmail.com>
2025-11-27 01:34:25 -0600

    PROGRAMA OCHO EXPLICACION PROFUNDA

--- ocho.cpp
+++ ocho.cpp
@@ -2,20 +2,25 @@
 using namespace std;
 int main()
 {
+//DECLARACION DE VARIABLES. X SIGNIFICA EL NUMERO DE NUMEROS QUE VAN A LEER.
 int x;
 int arreglo[100];
 cin>>x;
 
+//CICLO PARA LEER LOS X NUMEROS.
 for(int i=0; i<x; i++){
     cin>>arreglo[i];
 }
 
+// EL CONTANDOR SE VA UTILIZAR PARA CONTAR LOS OCHOS.
 int Contador = 0;
 
+// CICLO PARA CHECAR CUALES NUMEROS SON OCHOS Y CUALES NO.
 for(int i=0; i<=x; i++){
  if(arreglo[i]==8){
     Contador++;
 }
 }
+// IMPRIME EL NUMERO DE OCHOS QUE SE ENCONTRARON.
  cout<<Contador<<endl;
 }
